0f803089c5f978142e5aad35b6e4d83e309347fd
Douglaspereira77 <131231656+Douglaspereira77@users.noreply.github.com>
2024-11-13 11:40:19 +0300

    Initial commit from your app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Purchase Request System</h1>
+      <p>Welcome to the system!</p>
+    </div>
+  )
+}
+
+export default App
